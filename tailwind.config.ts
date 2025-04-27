b296806bb080d418e7c1d537a3924db2cc95e78f
welon0919 <welon0919@gmail.com>
2025-04-27 21:48:40 +0800

    Finished the left side of the navbar

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}", // 如果你是用 app/ 資料夾
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
